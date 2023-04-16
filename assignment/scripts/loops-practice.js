a4db5316f9c3f85fe3887eed973005f5c8f3efa6
Hamza Muhumed <hamzamuhumed2107@gmail.com>
2023-04-16 10:09:51 -0600

    Completed question 1 and did for loop to display numbers on the console

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -5,22 +5,30 @@ console.log('****** Loops Practice *******');
 console.log('---- 1. For loops ----');
 // Example: a for loop to console.log numbers from 0 to 3
 console.log('count from 0 to 3');
-// start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
-for (let i=0; i<4; i++) {  
-  console.log(i);
-}
-
+// start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)for (let i=0; i<4; i++) {  
+for (let i=0; i<4; i++){
+  console.log(i)
+};
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+for (let i=0; i<5; i++) {
+  console.log(i)
+};
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+for (let i=3; i<6; i++){
+  console.log(i)
+};
+// I changed i<5 to i<6 so I can see the number 5 on the console. Otherwise it would only display the number 3 and 4. 
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+
+// I made i<12 because I wanted to see the number 10 on the console instead of it stopping at 8. 
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
@@ -34,14 +42,21 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 console.log('Some stars:');
 
 
+
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
-console.log('Some stars using while:');
+console.log('Good stars:');
+
+// Changed the string 'Some stars' to 'Good stars' so I don't get confused on which loop it is I'm working on. 
+
+
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
+
+
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
